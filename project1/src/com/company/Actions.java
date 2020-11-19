bafd97e870c97ad75361e3dcdf18c9775d10a057
melikaabdi96 <melikaabdi_pc@yahoo.com>
2020-11-19 15:22:49 +0330

    add files

---
+++ project1/src/com/company/Actions.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Objects;
+
+public class Actions {
+    public void attack(Army army, int x, int y){};
+
+    public void move(String input , int n ,String armyName) {}
+
+}
